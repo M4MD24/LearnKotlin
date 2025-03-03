f191b02440809dfe2b9644fa683dca0b26104211
مُحَمَّد سَعْدَاوِيْ <m3md69@outlook.com>
2025-03-03 07:49:17 +0200

    Edit Things

--- src/_2_solve/_2_1_coder_hub/_2_1_1_challenges/_2_1_1_1_easy/_2_1_1_1_64_get_primes_between/GetPrimesBetweenExample.kt
+++ src/_2_solve/_2_1_coder_hub/_2_1_1_challenges/_2_1_1_1_easy/_2_1_1_1_64_get_primes_between/GetPrimesBetweenExample.kt
@@ -1,5 +1,7 @@
 package _2_solve._2_1_coder_hub._2_1_1_challenges._2_1_1_1_easy._2_1_1_1_64_get_primes_between
 
+import kotlin.math.sqrt
+
 fun main() = print(getPrimesBetween(readln().toInt(), readln().toInt()))
 
 private fun getPrimesBetween(startNumber: Int, endNumber: Int): MutableList<Int> {
@@ -10,7 +12,7 @@ private fun getPrimesBetween(startNumber: Int, endNumber: Int): MutableList<Int>
             return true
         if (number % 2 == 0 || number % 3 == 0)
             return false
-        for (index in 5..Math.sqrt(number.toDouble()).toInt() step 2)
+        for (index in 5..sqrt(number.toDouble()).toInt() step 2)
             if (number % index == 0)
                 return false
         return true
