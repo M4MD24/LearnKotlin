bbd96c292728f620df7b200c4d57a0f36d9ca425
مُحَمَّد سَعْدَاوِيْ <m3md69@outlook.com>
2025-03-22 19:18:15 +0200

    # Solve Problems Errors in Assiut Sheets

--- src/_2_solve/_2_2_codeforces/_2_2_1_assiut_sheet/_2_2_1_11_sheet_8/_2_2_1_11_k_prime_fibonacci/PrimeFibonacciExample.kt
+++ src/_2_solve/_2_2_codeforces/_2_2_1_assiut_sheet/_2_2_1_11_sheet_8/_2_2_1_11_k_prime_fibonacci/PrimeFibonacciExample.kt
@@ -1,19 +1,33 @@
 package _2_solve._2_2_codeforces._2_2_1_assiut_sheet._2_2_1_11_sheet_8._2_2_1_11_k_prime_fibonacci
 
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+private val INPUT = BufferedReader(InputStreamReader(System.`in`))
+private val OUTPUT = BufferedWriter(OutputStreamWriter(System.out))
+
 fun main() {
-    val countOfNumbers = readln().toInt()
-    repeat(countOfNumbers) {
-        val currentNumber = readln().toByte()
-        println(
-            if (isPrimeFibonacci(currentNumber))
-                "prime"
-            else
-                "not prime"
-        )
-    }
+    inputValues()
+    OUTPUT.flush()
 }
 
-private fun isPrimeFibonacci(currentNumber: Byte): Boolean {
-    val isPrime: List<Byte> = listOf(0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0)
-    return isPrime[currentNumber.toInt()] == 1.toByte()
+private fun inputValues() {
+    var countOfTestCases = INPUT.readLine().toInt()
+    while (countOfTestCases-- > 0)
+        OUTPUT.write(
+            (
+                    if (isPrime(INPUT.readLine().toByte()))
+                        "prime"
+                    else
+                        "not prime"
+                    ) +
+                    "\n"
+        )
+}
+
+private fun isPrime(number: Byte): Boolean {
+    val isPrime = byteArrayOf(0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0)
+    return isPrime[number.toInt()].toInt() == 1
 }
--- src/_2_solve/_2_2_codeforces/_2_2_1_assiut_sheet/_2_2_1_7_sheet_4/_2_2_1_7_z_clean_code/CleanCodeExample.kt
+++ src/_2_solve/_2_2_codeforces/_2_2_1_assiut_sheet/_2_2_1_7_sheet_4/_2_2_1_7_z_clean_code/CleanCodeExample.kt
@@ -1,24 +1,22 @@
 package _2_solve._2_2_codeforces._2_2_1_assiut_sheet._2_2_1_7_sheet_4._2_2_1_7_z_clean_code
 
-val cleanCode = StringBuilder()
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+private val INPUT = BufferedReader(InputStreamReader(System.`in`))
+private val TEXT = StringBuilder()
 
 fun main() {
     inputValues()
-    print(cleanCode)
+    println(TEXT)
 }
 
 private fun inputValues() {
-    var countOfEmpty = 0
     var open = true
     while (true) {
-        val line = readlnOrNull() ?: break
-        if (line.isEmpty()) {
-            countOfEmpty++
-        } else {
-            countOfEmpty = 0
+        val line = INPUT.readLine() ?: break
+        if (line.trim().isNotEmpty())
             open = isOpen(line, open)
-        }
-        if (countOfEmpty == 2) break
     }
 }
 
@@ -26,41 +24,44 @@ private fun isOpen(
     line: String,
     open: Boolean
 ): Boolean {
-    var isOpen = open
+    var currentOpen = open
     var lineAppended = false
-    val length = line.length
+    val lineLength = line.length
     var index = 0
-    while (index < length) {
-        when {
+    while (index < lineLength) {
+        if (
             line[index] == '/' &&
-                    index + 1 < length &&
-                    line[index + 1] == '/' &&
-                    isOpen -> break
-
+            index + 1 < lineLength &&
+            line[index + 1] == '/' &&
+            currentOpen
+        )
+            break
+        else if (
             line[index] == '/' &&
-                    index + 1 < length &&
-                    line[index + 1] == '*' &&
-                    isOpen -> {
-                index++
-                isOpen = false
-            }
-
+            index + 1 < lineLength &&
+            line[index + 1] == '*' &&
+            currentOpen
+        ) {
+            index++
+            currentOpen = false
+        } else if (
             line[index] == '*' &&
-                    index + 1 < length &&
-                    line[index + 1] == '/' &&
-                    !isOpen -> {
-                index++
-                isOpen = true
-            }
-
-            isOpen -> {
-                cleanCode.append(line[index])
-                lineAppended = true
-            }
+            index + 1 < lineLength &&
+            line[index + 1] == '/' &&
+            !currentOpen
+        ) {
+            index++
+            currentOpen = true
+        } else if (currentOpen) {
+            TEXT.append(line[index])
+            lineAppended = true
         }
         index++
     }
-    if (lineAppended && isOpen)
-        cleanCode.append("\n")
-    return isOpen
+    if (
+        lineAppended &&
+        currentOpen
+    )
+        TEXT.appendLine()
+    return currentOpen
 }
